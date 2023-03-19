6edd56c2713c5a6db213859918d480297018cb72
loki <maciej.romanski@payu.com>
2023-03-19 10:02:48 +0100

    Fix formatting

--- source/src/main/scala/pl/muninn/simple/validation/TypeImplicits.scala
+++ source/src/main/scala/pl/muninn/simple/validation/TypeImplicits.scala
@@ -2,12 +2,6 @@ package pl.muninn.simple.validation
 
 import pl.muninn.simple.validation.implicits._
 
-trait TypeImplicits
-    extends AnyTypeImplicits
-    with CollectionImplicits
-    with NumberImplicits
-    with OptionImplicits
-    with StringImplicits
-    with MapImplicits
+trait TypeImplicits extends AnyTypeImplicits with CollectionImplicits with NumberImplicits with OptionImplicits with StringImplicits with MapImplicits
 
 object TypeImplicits extends TypeImplicits
--- source/src/main/scala/pl/muninn/simple/validation/implicits/MapImplicits.scala
+++ source/src/main/scala/pl/muninn/simple/validation/implicits/MapImplicits.scala
@@ -8,11 +8,11 @@ trait MapImplicits {
   implicit class MapValidation[K, V](field: Validation[Map[K, V]]) {
     def containsKey(key: K): ValidationWithValidators[Map[K, V]]             = field.is(MapValidators.containsKey(key))
     def containsKeys(keys: Iterable[K]): ValidationWithValidators[Map[K, V]] = field.is(MapValidators.containsKeys(keys))
-    def empty: ValidationWithValidators[Map[K, V]]                         = field.is(MapValidators.empty[K, V])
-    def notEmpty: ValidationWithValidators[Map[K, V]]                      = field.is(MapValidators.notEmpty[K, V])
-    def minimalLength(expected: Int): ValidationWithValidators[Map[K, V]]  = field.is(MapValidators.minimalLength[K, V](expected))
-    def maximumLength(expected: Int): ValidationWithValidators[Map[K, V]]  = field.is(MapValidators.maximalLength[K, V](expected))
-    def expectedLength(expected: Int): ValidationWithValidators[Map[K, V]] = field.is(MapValidators.exactLength[K, V](expected))
+    def empty: ValidationWithValidators[Map[K, V]]                           = field.is(MapValidators.empty[K, V])
+    def notEmpty: ValidationWithValidators[Map[K, V]]                        = field.is(MapValidators.notEmpty[K, V])
+    def minimalLength(expected: Int): ValidationWithValidators[Map[K, V]]    = field.is(MapValidators.minimalLength[K, V](expected))
+    def maximumLength(expected: Int): ValidationWithValidators[Map[K, V]]    = field.is(MapValidators.maximalLength[K, V](expected))
+    def expectedLength(expected: Int): ValidationWithValidators[Map[K, V]]   = field.is(MapValidators.exactLength[K, V](expected))
   }
 
 }
